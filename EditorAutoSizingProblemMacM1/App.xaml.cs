4829e5e2ec5cf671f98ab1d900d790891b45274d
Martin Honnen <martin.honnen@gmx.de>
2022-03-19 23:52:13 +0100

    Add project files.

---
+++ EditorAutoSizingProblemMacM1/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EditorAutoSizingProblemMacM1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ EditorAutoSizingProblemMacM1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace EditorAutoSizingProblemMacM1;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    private void Test_Clicked(object sender, EventArgs e)
+    {
+		Editor1.Text += $"New lined added {DateTime.Now}{Environment.NewLine}";
+	}
+}
+
